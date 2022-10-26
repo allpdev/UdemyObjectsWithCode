a5d98c174f77c7db04d89b864824f6ffc679b9c8
allpdev <alpkctpdev@gmail.com>
2022-10-26 23:33:28 +0300

    Create Objects

--- UdemyObjectsWithCode/ViewController.swift
+++ UdemyObjectsWithCode/ViewController.swift
@@ -11,7 +11,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//Objects With Code
+        let  myLabel = UILabel()
+        myLabel.text = "Hi Sano"
+        myLabel.textAlignment = .center
+        myLabel.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        view.addSubview(myLabel)
+        
     }
 
 
