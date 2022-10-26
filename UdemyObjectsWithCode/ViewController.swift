5c1fe0a3af7478421bcf8b582ae754d9c4f4741b
allpdev <alpkctpdev@gmail.com>
2022-10-26 23:59:12 +0300

    Create & define buttons

--- UdemyObjectsWithCode/ViewController.swift
+++ UdemyObjectsWithCode/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var myLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,14 +19,30 @@ class ViewController: UIViewController {
         let width = view.frame.size.width
         let height = view.frame.size.height
         
-        let  myLabel = UILabel()
+        
         myLabel.text = "Hi Sano"
         myLabel.textAlignment = .center
         myLabel.frame = CGRect(x: width * 0.5 - width * 0.8 / 2, y: height * 0.5 - 50 / 2, width: width * 0.8, height: 50)
         view.addSubview(myLabel)
         
+//Button with Code
+        
+        let myButton = UIButton()
+        myButton.setTitle("Hi User", for: UIControl.State.normal)
+        myButton.setTitleColor(UIColor.blue, for: UIControl.State.normal)
+        myButton.frame = CGRect(x: width * 0.5 - 100, y: height * 0.6, width: 200, height: 100)
+        view.addSubview(myButton)
+        
+        myButton.addTarget(self, action: #selector(ViewController.myAction), for: UIControl.Event.touchUpInside)
+        
+        
+    }
+    
+    @objc func myAction() {
+        myLabel.text = "Hi alp"
     }
 
 
 }
+
 
